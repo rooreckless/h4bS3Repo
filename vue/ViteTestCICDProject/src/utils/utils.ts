18407ff29a46953f93e59877f54d0f4c6c33ef27
rooreckless <rooreckless@gmail.com>
2024-11-26 09:49:51 +0900

    testに通らない状態でのコミット

--- vue/ViteTestCICDProject/src/utils/utils.ts
+++ vue/ViteTestCICDProject/src/utils/utils.ts
@@ -4,5 +4,5 @@ export function getCurrentDateString(): string {
   const month = String(now.getMonth() + 1).padStart(2, "0"); // 月 (2桁)
   const day = String(now.getDate()).padStart(2, "0"); // 日 (2桁)
 
-  return `${year}-${month}-${day}`;
+  return `${year}-${month}XX-${day}`;
 }
